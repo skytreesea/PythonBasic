4f00f3126dbee4ac22f0f68150d2829138ada9b8
skytreesea <skytreesea@gmail.com>
2021-01-02 06:27:37 +0900

    add open function

--- basic_functions.py
+++ basic_functions.py
@@ -1,3 +1,11 @@
+# 2강 basic 5 강의
+# 파이썬의 변수의 유형 초 간단 정리: int, str, float, list, dict
+# 파이썬 초심자를 위한 기초 함수: 
+# 1. 출력: print
+# 2. 반복: for
+# 3. 조건문: if
+# 4. 함수지정: def  
+
 #출력하기 
 print("Hello, world")
 
@@ -5,6 +13,21 @@ print("Hello, world")
 print('Hello, world! I am %d years old. My name is %s.'%(33, 'Changhyun'))
 age = 36
 print('I am %d years old' % age)
+
+# 유형의 기본만 알아봅시다. 
+# 정수 
+print(type(3))
+# 부동소수 float
+print(type(3.5))
+# 문자 str
+print(type('나는 문자열입니다. string이라고 하고 파이썬에서는 str이라고 합니다.'))
+# 리스트 list
+print(type([2,3,3]))
+potal ={'naver':'네이버','daum':'다음','yahoo':'야후'}
+print(type(potal))
+print(potal['naver'])
+print(potal.keys())
+print(potal.values())
 
 # 문자와 숫자 결합 X, 결합이 같아야 합칠 수 있음
 print(2+3)
@@ -90,7 +113,7 @@ def gugu(number):
 # gugu 함수 실행
 gugu(5)
 
-# return과 print의 차이: 반환하다 개념
+# (중요) return과 print의 차이: 반환하다 개념
 #숫자로 된 리스트를 입력받아 평균을 출력함
 def average(int_list):
     k=0
@@ -108,11 +131,12 @@ def average(int_list):
         k = k + i
     return k/len(int_list)
 # return 받은 값이 표현되지 않아 아무 일 일어나지 않은 것처럼 보입니다. 
+average(the_list)
 
-def average(int_list):
-    k=0
-    for i in int_list:
-        k = k + i
-    return k/len(int_list)
 # 그러나 출력된 값으로는 계산할 수 없지만, return 값으로는 계산할 수 있습니다.
 print(average(the_list)+ 500)
+
+import os 
+print(os.listdir())
+os.chdir(r'D:\user\Documents\git hub\PythonBasic')
+print(os.listdir())
