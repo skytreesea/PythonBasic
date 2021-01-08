b66103278f5fe9ec2d72938f71d33b6616a5cc8c
skytreesea <skytreesea@gmail.com>
2021-01-09 06:26:23 +0900

    add the example of beautifulsoup

--- crawling/beautifulsoup_basic.py
+++ crawling/beautifulsoup_basic.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import requests
 url = 'https://www.naver.com/'
 req = requests.get(url)
+#기본명령
 soup = BeautifulSoup(req.text)
+#a태그 리스트로 반환
 a_list = soup.find_all('a')
+#텍스트만 출력
 print([i.text for i in a_list])
