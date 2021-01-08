519444e80aec8ed056a0940bc4a651e499e97197
Kim Changhyun <skytreesea@gmail.com>
2021-01-09 06:03:58 +0900

    Update selenium_basic_naver_suc.py

--- crawling/selenium_basic_naver_suc.py
+++ crawling/selenium_basic_naver_suc.py
@@ -4,5 +4,3 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(r'D:\user\Documents\chromedriver.exe')
 driver.get('https://www.naver.com')
-#오류메시지 대처
-#selenium.common.exceptions.WebDriverException: Message: 'chromedriver.exe"' executable needs to be i0
