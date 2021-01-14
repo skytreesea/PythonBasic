530f860e816af8be9418a6acf7836815fa065286
skytreesea <skytreesea@gmail.com>
2021-01-14 17:14:01 +0900

    for taping

--- crawling/articlebasic.py
+++ crawling/articlebasic.py
@@ -1,10 +1,28 @@
-from bs4 import BeautifulSoup as bs
+# 뷰티풀수프 상용어구
+from bs4 import BeautifulSoup  
 import requests,re
 #네이버 기사 url만 바꾸기
-url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=469&aid=0000571690'
+url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=100&oid=081&aid=0003155887'
 headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
 res = requests.get(url, headers = headers)
-soup = bs(res.text, 'lxml')
+soup = BeautifulSoup(res.text, 'lxml')
 
-for i in soup.find('strong',{'class':'media_end_summary'}):
-    print(i)
+# 요약본만 가져오기 
+# for i in soup.find('strong',{'class':'media_end_summary'}):
+#     print(i)
+# # 제목만 가져오기
+# for i in soup.find('h3',{'id':'articleTitle'}):
+#     print(i)
+
+# # 기사입력시간
+# for i in soup.find('span',{'class':'t11'}):
+#     print(i)
+
+#페이지 따라가서 크롤링하기 
+url2='https://finance.naver.com/sise/sise_index_time.nhn?code=KPI200&thistime=20210114170300&page='
+for i in range(1,3):
+    url = url2 + str(i)
+    headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
+    res = requests.get(url2, headers = headers)
+    soup = BeautifulSoup(res.text, 'lxml')
+    soup.find_all('table')
