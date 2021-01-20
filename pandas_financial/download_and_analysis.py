753877bc06d54e34ef53a5ee6722a7a7c9e4e5b3
skytreesea <skytreesea@gmail.com>
2021-01-20 09:32:21 +0900

    Simplify the download and analysis

--- pandas_financial/download_and_analysis.py
+++ pandas_financial/download_and_analysis.py
@@ -15,11 +15,4 @@ df = df.iloc[2:,:]
 # 잘라내고 남은 첫 행이 칼럼명이 됨
 df.columns = df.iloc[0]
 to_firms=pd.crosstab(df['지역'],df['공기업유형'])
-# sum()을 통해 '지역','공기업유형'별 합을 따로 구함 
-regionSum =to_firms.transpose().sum()
-typeSum =  to_firms.sum()
-# 행에 있는 것(지역)부터 합치고 회전시켜서 공기업유형 더함 
-new = pd.concat([to_firms, regionSum], axis=1 )
-new = pd.concat([new.transpose(), typeSum], axis=1)
-# 값을 클립보드로 복사함
-new.transpose().to_clipboard()
+to_firms.to_clipboard()
