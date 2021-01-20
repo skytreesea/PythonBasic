dd909b6046b3d2dc56fa9883aa3e47567722ff4d
skytreesea <skytreesea@gmail.com>
2021-01-20 13:10:02 +0900

    Update download_and_analysis.py

--- pandas_financial/download_and_analysis.py
+++ pandas_financial/download_and_analysis.py
@@ -11,8 +11,14 @@ with open(file_path, "wb") as file:
 
 df = pd.read_excel(file_path)
 # 윗 두 줄은 불필요하니 잘라내는 
-df = df.iloc[2:,:]
-# 잘라내고 남은 첫 행이 칼럼명이 됨
-df.columns = df.iloc[0]
+df.columns = df.iloc[2]
+df = df.iloc[3:,:]
 to_firms=pd.crosstab(df['지역'],df['공기업유형'])
-to_firms.to_clipboard()
+# 잘라내고 남은 첫 행이 칼럼명이 됨
+regionSum =to_firms.transpose().sum()
+typeSum =  to_firms.sum()
+# 행에 있는 것(지역)부터 합치고 회전시켜서 공기업유형 더함 
+new = pd.concat([to_firms, regionSum], axis=1 )
+new = pd.concat([new.transpose(), typeSum], axis=1)
+# 값을 클립보드로 복사함
+new.transpose().to_clipboard()
