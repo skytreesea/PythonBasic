26f0db865f9d4fbc687792b71b65de688ea0b906
Kim Changhyun <skytreesea@gmail.com>
2023-02-06 05:05:55 +0900

    Organizing the file structure of crawling file

--- crawling/paper search/paper_crawling_python google test.py
+++ crawling/paper search/paper_crawling_python google test.py
@@ -3,14 +3,14 @@
 from bs4 import BeautifulSoup  
 import requests 
 import re
-#네이버 기사 url만 바꾸기: 따옴표 안에 url을 붙여넣으세요.
-Name = input("권규상")
+#검색할 저자 이름
+Name = input("검색할 저자 이름을 입력하세요.")
 
 headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
  
 url = 'https://scholar.google.com/scholar?hl=ko&as_sdt=0%2C5&q='  + Name
 urls = []
 soup = BeautifulSoup(requests.get(url, verify=False, headers = headers).text, 'html.parser')
-# 각 상품정보에서 특정 정보를 리스트 형태로 모으기(데이터프레임 용)
+# 아직 아주 기본적인 코드만 짜놓음 추후 개발 예정
 print(soup.find_all('div',{"class":"gs_ri"}) )
  
