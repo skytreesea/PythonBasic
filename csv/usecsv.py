c535000b7271e5ab2fcd8e9a37c0d24e1c8a7c0a
Kim Changhyun <skytreesea@gmail.com>
2023-06-28 04:20:23 +0900

    utf-8-sig

--- csv/usecsv.py
+++ csv/usecsv.py
@@ -18,7 +18,7 @@ def opencsv(filename):
 # after import usecsv, then "writecsv('a.csv', a)
 
 def writecsv(filename, the_list):
-    with open(filename,'w',newline='') as f:
+    with open(filename,'w',newline='',encoding='utf-8-sig') as f:
         a=csv.writer(f, delimiter=',')
         a.writerows(the_list)
 
