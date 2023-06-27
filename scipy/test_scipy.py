0f321ef94aee01be1d04ae9cd331f2ff0be7e3b3
Kim Changhyun <skytreesea@gmail.com>
2023-06-28 05:02:38 +0900

    Create test_scipy.py

---
+++ scipy/test_scipy.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy import stats
+
+# Sample data
+x = np.array([1, 2, 4, 5, 8])
+y = np.array([2, 3, 4, 5, 6])
+
+# Perform linear regression
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+# Print the results
+print("Slope:", slope)
+print("Intercept:", intercept)
+print("R-squared:", r_value ** 2)
+print("P-value:", p_value)
+print("Standard error:", std_err)
