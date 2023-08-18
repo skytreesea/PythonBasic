49b36b71da08e5adeeb9d4978e15d0c8f975563a
Kim Changhyun <skytreesea@gmail.com>
2023-08-18 17:04:35 +0900

    update ai model

--- csv/stock analysis/stock_analysis_with_regression_ver3.py
+++ csv/stock analysis/stock_analysis_with_regression_ver3.py
@@ -1,15 +1,21 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import os 
+from sklearn.linear_model import LinearRegression
 os.chdir(r"C:\Users\skytr\OneDrive\문서\PythonBasic\csv\stock analysis\stocks")
 stocks = os.listdir()
 print(stocks)
 r = lambda  x : x.astype(float)
+ro = lambda  x : round(x,2)
+# Reshape the data into a 2-dimensional array.
+ch = lambda value : np.reshape(df[value].tolist(), (-1, 1))
 # major_index = ('Date', 'Prev_ror_x','ror_day_x','Prev_Close_x','Open_x','Close_x','rate_of_return_x')
 def create_df(name_of_stock_csv):
     df = pd.read_csv(name_of_stock_csv)
     x = r(df['Open'])
     y = r(df['Close'])
+    # 수익률 
     df['rate_of_return'] = (y - x)*100/x
     df['diff'] = y - x
     df['hl_diff'] = r(df['High']) - r(df['Low']) 
@@ -18,6 +24,7 @@ def create_df(name_of_stock_csv):
     df['Prev_Close'] = df['Close'].shift(1) 
     df['Prev_ror'] = df['rate_of_return'].shift(1) 
     df['ror_day'] = (df['Open']-df['Prev_Close'])*100/df['Prev_Close'] # 전날 종가 대비 다음날 시가 수익률
+    # 칼럼명 각각 지정 성공 
     df.columns = ['Date']+[col + "_"+name_of_stock_csv[:-4] for col in df.columns if col != "Date"]
     return df
 
@@ -27,17 +34,34 @@ for i in stocks[1:]:
     df = pd.merge(df,create_df(i), on="Date")
     
 print(df.head())
-# df = pd.merge(create_df('SOXL'), create_df('SOXS'), on="Date" )
-
 # print(df.sort_values(by="rate_of_return", ascending=False))
-df.to_clipboard()
-
+# df.to_clipboard()
+def reg(indep, dep):
+    model = LinearRegression()
+    x = ch(indep)
+    y = ch(dep)
+#회귀분석
+    model.fit(x,y)
+# 0행과 1행 삭제
+    r_squared = model.score(x, y)
+    if r_squared > .3:
+        print(f"독립변수: {indep}\n종속변수: {dep}\n변동계수: {ro (model.coef_[0][0])} 절편:{ro (model.intercept_[0])}, r제곱: {ro (r_squared)}\n")
+    
 #plt.plot(df['ror_day_x'][-50:],df['rate_of_return_y'][:50], '*')  
 #plt.xticks(rotation=45)
-
+df = df.drop(0)
 # plt.show()
-# 전날 수익 3% 이상 나고, 시가-종가 수익률 0보다 클때
-# print(df[(df['Prev_ror_x'] > 3) &(df['ror_day_x'] > 0)].sort_values(by='rate_of_return_x', ascending=False).loc[:, major_index][:10])
+# NVDA 전날 수익 3% 이상 났을 때 수익률
+# print(df[(df['Prev_ror_NVDA'] > 3)].loc[:, ('Date','Prev_ror_NVDA','rate_of_return_NVDA','Prev_ror_SOXL','rate_of_return_SOXL','rate_of_return_TQQQ')][:20].sort_values(by='Prev_ror_NVDA', ascending=False))
+indeps = ['Prev_ror_NVDA','Prev_ror_SOXL','Prev_ror_TQQQ']
+for i in df.columns:
+    for j in indeps:
+        if i != j:
+            try:    
+                reg(j,i) 
+            except:
+                print(i)
+        
 # tot = len(df)
 # num = len(df[df['rate_of_return_x']> 0 ])
 # num_2 = len(df[(df['rate_of_return_x']> 2) & (df['Prev_ror_x']> 2) ])
