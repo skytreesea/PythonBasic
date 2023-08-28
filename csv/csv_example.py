7deeabdcb00aed981619092adb8bf306744f5e7f
skytreesea <skytreesea@gmail.com>
2023-08-28 10:17:59 +0900

    delete some files

--- csv/csv_example.py
+++
@@ -1,4 +0,0 @@
-import csv, usecsv, os
-csv_test = [['국어', '영어','수학'], [90,80,100]]
-os.chdir(r'C:\Users\skytr\Documents\GitHub\PythonBasic\csv')
-usecsv.writecsv('score3.csv',csv_test)
--- csv/usecsv_test.py
+++
@@ -1,7 +0,0 @@
-import os 
-os.chdir(r'C:\Users\ERC\Documents\GitHub\PythonBasic\csv')
-import usecsv
-ad=[[1,2,3],['우리','나라','만세'],['나는','우리나라가','좋다']]
-usecsv.writecsv('new_write_file.csv',ad)
-total = usecsv.opencsv('new.csv')
-print(total)
